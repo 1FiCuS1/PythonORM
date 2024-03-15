5a8bd1127392980de8cf0023a7638eaebf644421
Мария Алексеева <12ficus12@gmail.com>
2024-03-15 15:13:14 +0400

    End?

--- models.py
+++ models.py
@@ -9,8 +9,6 @@ class Publisher(Base):
     id = sq.Column(sq.Integer, primary_key=True)
     name = sq.Column(sq.String(length=50), unique=True)
 
-    book_id = sq.Column(sq.Integer, sq.ForeignKey("book.id"), nullable=False)
-
     def __str__(self):
         return f'{self.id}: {self.name}'
 
@@ -21,10 +19,7 @@ class Book(Base):
     title = sq.Column(sq.String(length=50), nullable=False)
 
     publisher_id = sq.Column(sq.Integer, sq.ForeignKey("publisher.id"), nullable=False)
-    stock_id = sq.Column(sq.Integer, sq.ForeignKey("stock.id"), nullable=False)
-
     publisher = relationship("Publisher", backref="book")
-    stock = relationship("Stock", backref="book")
 
     def __str__(self):
         return f'{self.id}: {self.title}'
@@ -34,9 +29,6 @@ class Shop(Base):
 
     id = sq.Column(sq.Integer, primary_key=True)
     name = sq.Column(sq.String(length=50), nullable=False)
-    stock_id = sq.Column(sq.Integer, sq.ForeignKey("stock.id"), nullable=False)
-
-    stock = relationship("Stock", backref="shop")
 
     def __str__(self):
         return f'{self.id}: {self.name}'
@@ -46,12 +38,8 @@ class Stock(Base):
 
     id = sq.Column(sq.Integer, primary_key=True)
     count = sq.Column(sq.Integer)
-
     book_id = sq.Column(sq.Integer, sq.ForeignKey("book.id"), nullable=False)
     shop_id = sq.Column(sq.Integer, sq.ForeignKey("shop.id"), nullable=False)
-    sale_id = sq.Column(sq.Integer, sq.ForeignKey("sale.id"), nullable=False)
-
-    book = relationship("Book", backref="stock")
     shop = relationship("Shop", backref="stock")
     sale = relationship("Sale", backref="stock")
 
@@ -66,8 +54,6 @@ class Sale(Base):
     date_sale = sq.Column(sq.Date)
     count = sq.Column(sq.Integer)
     stock_id = sq.Column(sq.Integer, sq.ForeignKey("stock.id"), nullable=False)
-
-    stock = relationship("Stock", backref="sale")
 
     def __str__(self):
         return f'{self.id}: {self.price}'
